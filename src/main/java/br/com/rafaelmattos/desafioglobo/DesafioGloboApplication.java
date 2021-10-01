51a71b568300980e546f42ac6527503e27c72574
Rafael Mattos <rafaelrj@live.com>
2021-10-01 19:58:57 -0300

    Project created

---
+++ src/main/java/br/com/rafaelmattos/desafioglobo/DesafioGloboApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.desafioglobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGloboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelmattos/desafioglobo/DesafioGloboApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.desafioglobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGloboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
